acc2593e5769ec58cd8ea786275eb875cadee3fd
salvafern <salvador.fernandez@vliz.be>
2023-06-27 17:14:32 +0200

    docs: explain each fun, ERDDAP and the dataset_id naming

---
+++ R/biooracler-package.R
@@ -0,0 +1,115 @@
+#' @keywords internal
+"_PACKAGE"
+
+## usethis namespace: start
+## usethis namespace: end
+NULL
+
+#' @name ERDDAP
+#' @title Bio-Oracle ERDDAP Server
+#'
+#' @description
+#' The [Bio-Oracle](https://bio-oracle.org/) dataset is made available via an ERDDAP
+#' server hosted by the [Flanders Marine Institute (VLIZ)](https://www.vliz.be).
+#'
+#' ERDDAP is a data server that gives you simple, consistent way to download subsets
+#' of scientific datasets in common file formats and make graphs and maps. The Bio-Oracle
+#' ERDDAP installation is available online at:
+#'
+#' http://erddap.bio-oracle.org/erddap/
+#'
+#' ERDDAP offers a REST API that can be consumed by other software For instance,
+#' there is an R package that reads data from ERDDAP named [rerddap]. This package is
+#' the backbone behind `biooracler`: In fact, `biooracler` just wraps [rerddap] in a
+#' convenient way to only read data from the Bio-Oracle ERDDAP server. But the Bio-Oracle
+#' ERDDAP server is independent from the `biooracler` package.
+#'
+#' You are welcome to access the Bio-Oracle ERDDAP server in your preferred way,
+#' including via the [rerddap] R package. This will ease the combination of the
+#' Bio-Oracle dataset with several other ERDDAP servers.
+#'
+#' In addition, there is a Python extension that mimics the methods of `biooracler`. You
+#' can access it at:
+#'
+#' https://github.com/bio-oracle/pyo_oracle/
+#'
+#' Each dataset in the Bio-Oracle ERDDAP server is identified by a [?dataset_id].
+#'
+#' @return Returns a help page
+#'
+#' @examples \dontrun{?ERDDAP}
+NULL
+
+#' @name dataset_id
+#' @title Unique Identifier of a Bio-Oracle dataset in the ERDDAP Server
+#'
+#' @description
+#' The [Bio-Oracle](https://bio-oracle.org/) datasets have an unique identifier in the
+#' [?ERDDAP] server where they are hosted.
+#'
+#' Despite the title and summary of the dataset (See [list_layers()]) already describe
+#' the data included in the dataset, the `dataset_id` follow a certain naming that
+#' indicate the information included inside. For instance:
+#'
+#' -`phyc_baseline_2000_2020_depthsurf`
+#' -`thetao_ssp119_2020_2100_depthmean`
+#' -`thetao_baseline_2020_2019_depthsurf`
+#'
+#' ### Environmental variable
+#' Indicated the first part of the naming (e.g. `phyc`, `thetao`). The equivalences are:
+#' - `chl` = Chlorophyll
+#' - `clt` = Total Cloud Fraction
+#' - `dfe` = Dissolved Iron
+#' - `mlotst` = Mixed Layer Depth
+#' - `no3` = Nitrate
+#' - `o2` = Dissolved Molecular Oxygen
+#' - `ph` = pH
+#' - `phyc` = Total Phytoplankton
+#' - `phyc` = Phytoplankton
+#' - `po4` = Phosphate
+#' - `si` = Silicate
+#' - `siconc` = Sea Ice Cover
+#' - `sithick` = Sea Ice Thickness
+#' - `so` = Salinity
+#' - `sws` = Sea Water Speed
+#' - `tas` = Air Temperature
+#' - `terrain` = Terrain
+#' - `thetao` = Ocean Temperature
+#'
+#' ### Time: Present/Past baseline or Future CMIIP6 Climate Change Projection projection
+#'
+#' The part with `baseline` or `ssp119` indicates if the layer contains present or past
+#' decades, or future climate change projections under the provisions of the
+#' [Intergovernmental Panel on Climate Change](https://www.ipcc.ch/) (IPCC) 6th report. This report features the
+#' 6th generation of [Coupled Model Intercomparison Projects](https://www.carbonbrief.org/qa-how-do-climate-models-work#cmip) (CMIP6)
+#' and describes a few [Shared Socioeconomic Pathways](https://www.carbonbrief.org/explainer-how-shared-socioeconomic-pathways-explore-future-climate-change/) (SSPs), which are different
+#' climate change scenarios.
+#'
+#' The Bio-Oracle dataset predicts the environmental variables for 8 future decades
+#' (2020-2100) and for 6 SSPs scenarios: `ssp119`, `ssp126`, `ssp245`, `ssp370`, `ssp460` and `ssp585`.
+#'
+#' On the other hand, `baseline` refers to the conditions of the environmental variable
+#' for the decades of 2000 to 2010 and 2010 to 2020, as interpolated from the
+#' [Copernicus Marine Service](https://marine.copernicus.eu/) data. It is named baseline
+#' as this interpolated layers are the baseline to model the future predictions.
+#'
+#' ### Surface or Benthic
+#'
+#' The Bio-Oracle datasets includes modelled environmental data for the surface sea water
+#' layer (`depthsurf`), but also for the bottom of the ocean. Note that all datasets with
+#' the subfixes `depthmin`, `depthmean` and `depthmax` refer to the benthic layer and
+#' *never refer to different depths of the ocean*. In other words: Bio-Oracle offers you
+#' data from the ocean surface or the bottom, but *never different levels of depth*.
+#'
+#' The subfixes `min`, `mean` and `max` refer to the depth value used during modelling:
+#' Bio-Oracle uses bathymetry data from [GEBCO](https://www.gebco.net/), which has a
+#' higher resolution than the resolution of Bio-Oracle. Hence, for each grid cell of
+#' the Bio-Oracle dataset there are more than one value of bathymetry Three different
+#' bathymetry values were selected to fit the models: either the minimal value
+#' (`depthmin`), an average of all the bathymetry values (`depthmean`) or the maximum
+#' value (`depthmax`).
+#'
+#' @return Returns a help page
+#'
+#' @examples \dontrun{?dataset_id}
+NULL
--- R/main.R
+++ R/main.R
@@ -1,20 +1,32 @@
-#' Downloads a griddap dataset from an ERDDAP server
+#' Downloads Bio-Oracle layers from the Bio-Oracle ERDDAP server
 #'
-#' @param dataset
-#' @param variables
-#' @param constraints
-#' @param fmt
-#' @param directory
-#' @param verbose
+#' @param dataset_id An unique identifier of the dataset in [?ERDDAP]. See [?dataset_id].
+#' @param variables Variable names to retrieve from the dataset. If NULL (default), all variables are retrieved.
+#' @param constraints List to query on dimensions time, latitude and longitude
+#' @param fmt Output format. One of 'csv', 'nc' or 'raster' (default). See details.
+#' @param directory Local directory where files are cached. If NULL (default), it is as in [rerddap::griddap]
+#' @param verbose Logical: Show info messages? Default is TRUE
 #'
-#' @return
+#' @details
+#'
+#' All Bio-Oracle layers retrieved are downloaded to a cache directory and later read
+#' into memory in R. Depending on the output format selected on the `fmt` argument you
+#' will obtain:
+#'
+#' - `csv`: An object of class `griddap_csv`. In essence, a `data.frame` with extra info.
+#'   A csv file will be downloaded to the cache directory. See `?rerrdap::griddap`.
+#' - `nc`: An object of class `griddap_nc`. This is the unclassed output of [ncdf4::nc_open()].
+#'  A [NetCDF](https://www.unidata.ucar.edu/software/netcdf/) file will be downloaded to the cache directory. See `?rerrdap::griddap`.
+#' - `raster`: An object of class `SpatRast`, obtained by reading the [NetCDF](https://www.unidata.ucar.edu/software/netcdf/) file with `terra::rast`.
+#'
+#' @return A downloaded `csv` or `nc` file and an object of class `griddap_csv`, `griddap_nc` or `SpatRast`
 #' @export
 #'
-#' @seealso [list_layers]
+#' @seealso [list_layers()], [info_layer()], [?ERDDAP], [?dataset_id]
 #'
 #' @examples \dontrun{
 #' # Test variables
-#' datasetid = "tas_baseline_2000_2020_depthsurf"
+#' dataset_id = "tas_baseline_2000_2020_depthsurf"
 #' time = c('2001-01-01T00:00:00Z', '2010-01-01T00:00:00Z')
 #' latitude = c(10, 20)
 #' longitude = c(120, 130)
@@ -23,21 +35,31 @@
 #' names(constraints) = c("time", "latitude", "longitude")
 #'
 #' # Test call
-#' download_dataset(datasetid, variables, constraints)
+#' download_dataset(dataset_id, variables, constraints)
 #' }
-download_dataset = function(dataset,
-                                    variables,
-                                    constraints,
-                                    fmt="nc",
-                                    directory=FALSE,
-                                    verbose=TRUE,
-                                    debug=FALSE
+download_dataset = function(dataset_id,
+                                    variables = NULL,
+                                    constraints = list(),
+                                    fmt = "raster",
+                                    directory = NULL,
+                                    verbose = TRUE,
+                                    debug = FALSE
 ) {
+  # Assertions
+  checkmate::assert_character(dataset_id, len = 1)
+  checkmate::assert_choice(dataset_id, list_layers()$dataset_id)
+  checkmate::assert_list(constraints, min.len = 1, any.missing = FALSE, unique = TRUE)
+  checkmate::assert_character(fmt, len = 1)
+  checkmate::assert_choice(fmt, c("csv", "nc", "raster"))
+  checkmate::assert_logical(verbose, len = 1)
+  checkmate::assert_logical(debug, len = 1)
 
+  # Config
   printer = function(message, verbose=parent.frame()$verbose) {
     if(verbose) { message(message) }
   }
 
+  is_raster <- FALSE
   if(fmt == "raster"){
     is_raster <- TRUE
     fmt <- "nc"
@@ -46,34 +68,29 @@ download_dataset = function(dataset,
   # Args to be passed to griddap call later on
   docallargs = list()
   docallargs[["fmt"]] = fmt
-  out = rerddap::info(datasetid=dataset, url = erddap.bio_oracle.org())
+  out = rerddap::info(datasetid=dataset_id, url = erddap.bio_oracle.org())
   docallargs[["datasetx"]] = out
 
-  printer(sprintf("Selected dataset %s.", dataset))
-  printer(sprintf("Dataset info available at: %s/griddap/%s.html", out$base_url, dataset))
-
+  printer(sprintf("Selected dataset %s.", dataset_id))
+  printer(sprintf("Dataset info available at: %s/griddap/%s.html", out$base_url, dataset_id))
 
   # Setting constraints that match
   for (constraint in names(constraints)) {
-    if (constraint %in% names(out$alldata)) {
-      docallargs[[constraint]]= get(constraint, constraints)
-    }
+    docallargs[[constraint]]= get(constraint, constraints)
   }
 
   # The same for variables
-  valid_variables = c()
-  for (variable in variables) {
-    if (variable %in% out$variables$variable_name) {
-      valid_variables = c(valid_variables, variable)
+  if (!is.null(variables)) {
+    for(variable in variables){
+      checkmate::assert_choice(variable, out$variables$variable_name)
     }
+    docallargs[["fields"]] = variables
+    printer(sprintf("Selected %s variables: %s", length(variables), toString(variables)))
   }
 
-  if (length(valid_variables) > 0) {docallargs[["fields"]] = valid_variables}
-  printer(sprintf("Selected %s variables: %s", length(valid_variables), toString(valid_variables)))
-
   # Set directory for storing data
-
-  if (!isFALSE(directory)) {
+  if (!is.null(directory)) {
+    checkmate::assert_directory_exists(directory, "w")
     cache_dir <- hoardr::hoard()
     cache_dir$cache_path_set(full_path=directory)
     docallargs[["store"]] = rerddap::disk(cache_dir$cache_path_get())
@@ -85,7 +102,7 @@ download_dataset = function(dataset,
   }
 
   # Call
-  res = do.call(rerddap::griddap, docallargs)
+  res = suppressMessages(do.call(rerddap::griddap, docallargs))
 
   if(is_raster) res <- griddap_to_terra(res)
 
@@ -93,14 +110,15 @@ download_dataset = function(dataset,
 }
 
 griddap_to_terra <- function(res){
-  terra::rast(x$summary$filename)
+  terra::rast(res$summary$filename)
 }
 
 
 
 .list_layers <- function(..., simplify = TRUE){
+  checkmate::assert_logical(simplify, len = 1)
 
-  # Retrieve either full list of layers or loop-up by free-text search
+  # Retrieve either full list of layers or look-up by free-text search
   if(missing(...)){
     layers <- rerddap::ed_datasets(which = "griddap", url = erddap.bio_oracle.org())
   }else{
@@ -111,18 +129,18 @@ griddap_to_terra <- function(res){
 
   # Aftermath
   layers <- colnames_normalize(layers)
-  if(simplify) layers <- simplify(layers)
+  if(simplify) layers <- do_simplify(layers)
 
   layers
 }
 
 colnames_normalize <- function(df){
-  colnames(df) <- df %>% colnames() %>% tolower()
+  colnames(df) <- tolower(colnames(df))
   colnames(df) <- gsub(".","_", colnames(df), fixed = TRUE)
   df
 }
 
-simplify <- function(df){
+do_simplify <- function(df){
   cols_desired <- c("dataset_id", "title", "summary")
   cols_desired_exist <- all(cols_desired %in% colnames(df))
 
@@ -136,16 +154,44 @@ simplify <- function(df){
 #' List the layers in the Bio-Oracle dataset
 #'
 #' @param ... Free text search or pass params to [rerddap::ed_search_adv]
+#' @param simplify Logical. Return only basic information? Default is TRUE. See format section.
 #'
-#' @return a data frame with the dataset ID that can be later passed to [download_layers]
-#' and further information
+#' @format ## `list_layers()`
+#' A data frame with at least three columns
+#' \describe{
+#'   \item{dataset_id}{Unique identifier of the Bio-Oracle layer, see [?dataset_id]. Use in [download_dataset()] or [info_layer()]}
+#'   \item{title}{Title of the Bio-Oracle layer}
+#'   \item{summary}{Description of the Bio-Oracle layer}
+#'   \item{...}{More columns, output from [rerddap::ed_search_adv()]}
+#' }
+#'
+#' @return A data frame with the dataset ID that can be later passed to [download_dataset()]
 #' @export
 #'
-#' @seealso [download_layers]
+#' @seealso [download_layers()], [?ERDDAP], [?dataset_id]
 #'
 #' @examples \dontrun{
 #' list_layers()
 #' list_layers("Ocean Temperature 2100")
+#' list_layers("Ocean Temperature 2100", simplify = FALSE)
 #' list_layers("thetao_ssp119_2020_2100_depthmean")
 #' }
 list_layers <- memoise::memoise(.list_layers)
+
+
+#' Gets detailed information about a single Bio-Oracle layer
+#'
+#' @param dataset_id
+#'
+#' @return An object of class [info] from the [rerddap] package
+#' @export
+#'
+#' @seealso [list_layers()], [download_dataset()], [?ERDDAP], [?dataset_id]
+#'
+#' @examples \dontrun{
+#' info_layer("thetao_ssp119_2020_2100_depthmean")
+#' }
+info_layer <- function(dataset_id){
+  checkmate::assert_character(dataset_id, len = 1)
+  rerddap::info(dataset_id, erddap.bio_oracle.org())
+}
