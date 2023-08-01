3da856785560dcc259cd3d2846b853c4fa7128b4
salvafern <salvador.fernandez@vliz.be>
2023-08-01 11:46:34 +0200

    test: update download test + new tests

--- tests/testthat/test-download.R
+++ tests/testthat/test-download.R
@@ -1,32 +1,61 @@
+# Config
+dataset_id = "tas_baseline_2000_2020_depthsurf"
+time = c('2001-01-01T00:00:00Z', '2010-01-01T00:00:00Z')
+latitude = c(10, 11)
+longitude = c(120, 121)
+variables = c("tas_mean")
+constraints = list(time, latitude, longitude)
+names(constraints) = c("time", "latitude", "longitude")
+
+# Run
 test_that("download dataset works", {
   skip_if_offline()
   skip_on_cran()
 
-  # Test variables
-  url = "https://erddap-test.emodnet.eu/erddap/"
-  datasetid = "biooracle_dmo_ds"
-  time = c('2001-01-01T00:00:00Z', '2010-01-01T00:00:00Z')
-  latitude = c(10, 20)
-  longitude = c(120, 130)
-  variables = c("o2_mean")
-  constraints = list(time, latitude, longitude)
-  names(constraints) = c("time", "latitude", "longitude")
-
   # Test call
-  server_is_working <- httr::status_code(httr::HEAD(url)) == 200
-  if(server_is_working){
-    test <- download_griddap_dataset(url, datasetid, variables, constraints)
+  test <- download_dataset(dataset_id, variables, constraints, fmt = "csv")
+    expect_type(test, "list")
+    expect_s3_class(test, "griddap_csv")
+    expect_s3_class(test, "data.frame")
 
+  test <- download_dataset(dataset_id, variables, constraints, fmt = "nc")
     expect_type(test, "list")
     expect_s3_class(test, "griddap_nc")
-    expect_s3_class(test$data, "data.frame")
+    expect_s3_class(test, "nc")
 
-    # Test download to custom dir
-    temp_dir <- normalizePath(tempdir())
-    test_dir <- download_griddap_dataset(directory = temp_dir, url, datasetid, variables, constraints)
+  test <- download_dataset(dataset_id, variables, constraints, fmt = "raster")
+    expect_type(test, "S4")
+    expect_s4_class(test, "SpatRaster")
 
-    out_dir <- normalizePath(dirname(test_dir$summary$filename))
+
+  # Test download to custom dir, and nc response
+  temp_dir <- normalizePath(tempdir())
+  test_dir <- download_dataset(dataset_id, variables, constraints, fmt="nc", directory = temp_dir)
+    expect_s3_class(test_dir, "griddap_nc")
+    expect_s3_class(test_dir$data, "data.frame")
+
+  out_dir <- normalizePath(dirname(test_dir$summary$filename))
     expect_equal(out_dir, temp_dir)
-  }
-
 })
+
+test_that("List layers work", {
+  expect_true(memoise::is.memoised(list_layers))
+
+  test <- list_layers()
+  expect_s3_class(test, "data.frame")
+  expect_equal(ncol(test), 3)
+
+  test <- list_layers("temperature")
+  expect_s3_class(test, "data.frame")
+
+  test <- list_layers("temperature", simplify = FALSE)
+  expect_s3_class(test, "data.frame")
+  expect_gt(ncol(test), 3)
+})
+
+test_that("Info layer works", {
+  test <- info_layer(dataset_id)
+  expect_type(test, "list")
+  expect_s3_class(test, "info")
+})
+
